73e240470904083ae2b5425a631f1a7a71ec165b
Shubham Saha <imshubhamsaha@gmail.com>
2024-08-25 00:30:26 +0530

    Change in approach for avoiding number greater than 32-bit

--- basic-maths/reverse-integer.js
+++ basic-maths/reverse-integer.js
@@ -3,17 +3,23 @@
  * @return {number}
  */
 var reverse = function (x) {
-  const sign = x < 0 ? -1 : 1;
-  let num = Math.abs(x);
+  let signed = x < 0;
+
+  if (x < 0) x *= -1;
   let rev_num = 0;
+  while (x > 0) {
+    let digit = x % 10;
+    x = Math.floor(x / 10);
 
-  while (num > 0) {
-    rev_num = rev_num * 10 + (num % 10);
-    num = Math.floor(num / 10);
+    if (
+      rev_num > Math.floor((2 ** 31 - 1) / 10) ||
+      (rev_num === Math.floor((2 ** 31 - 1) / 10) && (2 ** 31 - 1) % 10 < digit)
+    )
+      return 0;
+    rev_num = rev_num * 10 + digit;
   }
-  if (rev_num * sign < -Math.pow(2, 31) || rev_num * sign > Math.pow(2, 31) + 1)
-    return 0;
-  return rev_num * sign;
-};
 
+  return signed ? -rev_num : rev_num;
+};
+//This function avoids the 64 digit integer;
 //TC --> O(log10(num)+ 1), SC --> O(1)
