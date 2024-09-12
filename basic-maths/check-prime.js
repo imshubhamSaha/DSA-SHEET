eeda14034d286f48ff74242df1384c864ca043be
Shubham Saha <imshubhamsaha@gmail.com>
2024-09-12 22:35:29 +0530

    check prime in squareroot time

--- basic-maths/check-prime.js
+++ basic-maths/check-prime.js
@@ -5,15 +5,19 @@
 var checkPrime = function (n) {
   let factors = 0;
 
-  for (let i = 1; i <= n; i++) if (n % i === 0) factors++;
-
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      factors++;
+      if (n / i !== i) factors++;
+    }
+  }
   if (factors === 2) return true;
 
   return false;
 };
 
 /**
- * TIME COMPLEXITY --> O(N)
+ * TIME COMPLEXITY --> O(SQRT(N))
  * SPACE COMPLEXITY --> CONSTANT
  */
-console.log(checkPrime(131));
+console.log(checkPrime(17));
